72db8dd5ee6cce8139acd0731cd1242a7b0020d7
Stephan Linkel <251381+norganos@users.noreply.github.com>
2024-02-12 09:48:37 +0100

    launched micronaut

---
+++ src/test/kotlin/de/linkel/privpanel/DashboardTest.kt
@@ -0,0 +1,19 @@
+package de.linkel.privpanel
+import io.micronaut.runtime.EmbeddedApplication
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import jakarta.inject.Inject
+
+@MicronautTest
+class DashboardTest {
+
+    @Inject
+    lateinit var application: EmbeddedApplication<*>
+
+    @Test
+    fun testItWorks() {
+        Assertions.assertTrue(application.isRunning)
+    }
+
+}
